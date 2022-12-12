0f153d2212e102f4a43ca4471fb060c65e9097e7
Юлия <jullka5@mail.ru>
2022-12-12 15:26:44 +0300

    Initial commit

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -5,13 +5,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
+
+    public String generateDate(int myDate) {
+        return LocalDate.now().plusDays(myDate).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
     @BeforeEach
     void setUp() {
@@ -22,45 +28,41 @@ public class CardDeliveryTest {
 
     @Test
     void shouldSuccessfulCompletionOfTheCardOrderForm1() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Омск");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("Петрова Анна");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
         $(By.className("button")).click();
-        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + myDate), Duration.ofSeconds(15000));
+        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + generateDate(3)), Duration.ofSeconds(15000));
     }
 
     @Test
     void shouldSuccessfulCompletionOfTheCardOrderForm2() {
-        String myDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(4));
         $("[data-test-id='name'] input").setValue("Мамин-Сибиряк Дмитрий");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
         $(By.className("button")).click();
-        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + myDate), Duration.ofSeconds(15000));
+        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + generateDate(4)), Duration.ofSeconds(15000));
     }
 
     @Test
     void shouldSuccessfulCompletionOfTheCardOrderForm3() {
-        String myDate = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(5));
         $("[data-test-id='name'] input").setValue("Орудж бей Байат");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
         $(By.className("button")).click();
-        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + myDate), Duration.ofSeconds(15000));
+        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + generateDate(5)), Duration.ofSeconds(15000));
     }
 
     @Test
     void shouldTestUnhappyPathCity1() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Имануэль");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
@@ -70,9 +72,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathCity2() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Саяногорск");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Имануэль");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
@@ -82,9 +83,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathCity3() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Moscow");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Имануэль");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
@@ -94,9 +94,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathCity4() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("1111");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Имануэль");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
@@ -106,9 +105,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathDate1() {
-        String myDate = LocalDate.now().plusDays(0).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(0));
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
@@ -118,9 +116,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathDate2() {
-        String myDate = LocalDate.now().plusDays(-10).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(-10));
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
@@ -141,9 +138,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathName1() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("Ivanov Ivan");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
@@ -153,9 +149,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathName2() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
@@ -165,9 +160,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathName3() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("11111");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
@@ -177,9 +171,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathName4() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("1!л -");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         $("[data-test-id='agreement']").click();
@@ -189,9 +182,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathPhone1() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("79333333333");
         $("[data-test-id='agreement']").click();
@@ -201,9 +193,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathPhone2() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("");
         $("[data-test-id='agreement']").click();
@@ -213,9 +204,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathPhone3() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("793333333333");
         $("[data-test-id='agreement']").click();
@@ -225,9 +215,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathPhone4() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+793333333333");
         $("[data-test-id='agreement']").click();
@@ -237,9 +226,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathPhone5() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("д79333333333+");
         $("[data-test-id='agreement']").click();
@@ -249,9 +237,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathPhone6() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("Иванов Иван");
         $("[data-test-id='phone'] input").setValue("+7933-333-33-33");
         $("[data-test-id='agreement']").click();
@@ -261,9 +248,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestUnhappyPathCheckbox() {
-        String myDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='city'] input").setValue("Омск");
-        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='date'] input").sendKeys(generateDate(3));
         $("[data-test-id='name'] input").setValue("Петрова Анна");
         $("[data-test-id='phone'] input").setValue("+79333333333");
         //$("[data-test-id='agreement']").click();
